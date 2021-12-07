f5bbdf68d07aad6ab49c2d89ff8fe1c3cee44ecb
neiro11 <67793493+neiro11@users.noreply.github.com>
2021-12-07 12:45:04 +0300

    Add files via upload

---
+++ zache1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m = 5;
+            int n = 5;
+            int[ , ] number = new int[m,n];
+            Random rnd = new Random();
+            int z = 0 ;
+            int summ = 0;
+
+            for(int i = 0; i < m ; i++) // первый
+            {
+                for (int k = 0; k < 1; k++)
+                {
+                    number[i, k] = rnd.Next(19, 50);
+                    if (number[i, k] % 2 != 0)
+                    {
+                        z += 1;
+                        summ += number[i, k];
+                    }
+                }
+                
+            }
+
+            double arith = summ / z;
+            Console.WriteLine("первый столбец");
+            Console.WriteLine(arith);
+            z = 0;
+            summ = 0;
+            arith = 0;
+            for (int i = 0; i < m; i++) // второй
+            {
+                for (int k = 0; k < 1; k++)
+                {
+                    number[i, k] = rnd.Next(19, 50);
+                    if (number[i, k] % 2 != 0)
+                    {
+                        z += 1;
+                        summ += number[i, k];
+                    }
+                }
+
+            }
+            arith = summ / z;
+            Console.WriteLine("второй столбец");
+            Console.WriteLine(arith);
+            z = 0;
+            summ = 0;
+            arith = 0;
+
+            for (int i = 0; i < m; i++)  // третий
+            {
+                for (int k = 0; k < 1; k++)
+                {
+                    number[i, k] = rnd.Next(19, 50);
+                    if (number[i, k] % 2 != 0)
+                    {
+                        z += 1;
+                        summ += number[i, k];
+                    }
+                }
+
+            }
+            arith = summ / z;
+            Console.WriteLine("третий столбец"); 
+            Console.WriteLine(arith);
+            z = 0;
+            summ = 0;
+            arith = 0;
+
+            for (int i = 0; i < m; i++) // четвертый
+            {
+                for (int k = 0; k < 1; k++)
+                {
+                    number[i, k] = rnd.Next(19, 50);
+                    if (number[i, k] % 2 != 0)
+                    {
+                        z += 1;
+                        summ += number[i, k];
+                    }
+                }
+
+            }
+            arith = summ / z;
+            Console.WriteLine("четвертый столбец");
+            Console.WriteLine(arith);
+            z = 0;
+            summ = 0;
+            arith = 0;
+
+            for (int i = 0; i < m; i++) // пятый
+            {
+                for (int k = 0; k < 1; k++)
+                {
+                    number[i, k] = rnd.Next(19, 50);
+                    if (number[i, k] % 2 != 0)
+                    {
+                        z += 1;
+                        summ += number[i, k];
+                    }
+                }
+
+            }
+            arith = summ / z;
+            Console.WriteLine("пятый столбец");
+            Console.WriteLine(arith);
+            Console.ReadKey();
+
+            
+
+        }
+    }
+}
---
+++ zachet2.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            
+            int k = 0;
+            Console.WriteLine("ВВЕДИТЕ n ");
+            n = Convert.ToInt32(Console.ReadLine());
+            n = n * 30;
+            n += 2;
+            if (n <= 31)
+                k = 1;
+            if (n <= 28 + 31 && n > 31)
+                k = 2;
+            if (n <= 31 + 28 + 31 && n > 28 + 31)
+                k = 3;
+            if (n <= 28 + 31 + 31 + 30 && n > 31 + 28 + 31)
+                k = 4;
+            if (n <= 28+31+31+30+31 && n > 28 + 31 + 31 + 30)
+                k = 5;
+            if (n <= 28+31+31+30+31+30 && n >28+31+31+30+31)
+                k = 6;
+            if (n <= 28+31+31+30+31+30+31 && n >28+31+31+30+31+30)
+                k = 7;
+            if (n <= 28+31+31+30+31+30+31+30 && n >28+31+31+30+31+30+31)
+                k = 8;
+            if (n <= 28 + 31 + 31 + 30 + 31 + 30 + 31 + 30 + 31 && n >28 + 31 + 31 + 30 + 31 + 30 + 31 + 30 + 31)
+                k = 9;
+            if (n <= 28 + 31 + 31 + 30 + 31 + 30 + 31 + 30 + 31 + 30 && n > 28 + 31 + 31 + 30 + 31 + 30 + 31 + 30 + 31)
+                k = 10;
+            if (n <= 28 + 31 + 31 + 30 + 31 + 30 + 31 + 30 +31 +30 && n >28 + 31 + 31 + 30 + 31 + 30 + 31 + 30+31 +30)
+                k = 11;
+            if (n <= 365 && n > 28 + 31 + 31 + 30 + 31 + 30 + 31 + 30 +31 +30 && n >28 + 31 + 31 + 30 + 31 + 30 + 31 + 30+31 +30)
+                k = 12;
+
+
+            if (k == 1)
+                Console.WriteLine("Январь");
+            if (k == 2)
+                Console.WriteLine("Февраль");
+            if (k == 3)
+                Console.WriteLine("Март");
+            if (k == 4)
+                Console.WriteLine("Апрель");
+            if (k == 5)
+                Console.WriteLine("Май");
+            if (k == 6)
+                Console.WriteLine("Июнь");
+            if (k == 7)
+                Console.WriteLine("июль");
+            if (k == 8)
+                Console.WriteLine("август");
+            if (k == 9)
+                Console.WriteLine("сентябрь");
+            if (k == 10)
+                Console.WriteLine("октябрь");
+            if (k == 11)
+                Console.WriteLine("ноябрь");
+            if (k == 12)
+                Console.WriteLine("декабрь");
+            Console.ReadKey();
+        }
+    }
+}
